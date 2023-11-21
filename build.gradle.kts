326057c5ac09d18373253da5d02be8aadfb7aa5c
zaleslaw <zaleslaw.sin@gmail.com>
2023-11-21 13:31:26 +0100

    Added skeleton, phase 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:dataframe:0.12.0")
-    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.5.0-rc-2")
+    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.5.0-rc-3")
     implementation ("org.jetbrains.kotlinx:kandy-api:0.5.0-rc-2")
     testImplementation(kotlin("test"))
 }
