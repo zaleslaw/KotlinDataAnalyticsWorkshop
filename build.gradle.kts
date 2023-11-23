f7a339abd8a62daa43561ebb0a54b0b0e051b763
zaleslaw <zaleslaw.sin@gmail.com>
2023-11-23 15:47:09 +0100

    Updated kandy dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:dataframe:0.12.0")
     implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.5.0-rc-3")
-    implementation ("org.jetbrains.kotlinx:kandy-api:0.5.0-rc-2")
+    implementation ("org.jetbrains.kotlinx:kandy-api:0.5.0-rc-3")
     implementation("org.jetbrains.kotlinx:kotlin-deeplearning-api:0.5.1")
     implementation("org.jetbrains.kotlinx:kotlin-deeplearning-impl:0.5.1")
     implementation("org.jetbrains.kotlinx:kotlin-deeplearning-onnx:0.5.1")
