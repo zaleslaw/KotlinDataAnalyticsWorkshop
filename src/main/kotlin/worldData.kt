08193db17a976e1160505310d2dc190412811c37
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2023-11-24 19:34:54 +0100

    Added a task set

--- src/main/kotlin/worldData.kt
+++ src/main/kotlin/worldData.kt
@@ -6,4 +6,16 @@ import org.jetbrains.kotlinx.dataframe.io.readCSV
 fun main() {
     var df = DataFrame.readCSV(fileOrUrl = "src/main/resources/world-data-2023.csv", delimiter = ',')
     df.head(10).print()
+
+    // **Task 1: Find top-10 countries by Total CO2 emissions**
+
+    // **Task 2: Build a Bar Plot**
+
+    // **Task 3: Find top-10 countries by Total CO2 emissions per Person**
+
+    // **Task 4: Build a Bar Gradient for absolute CO2 Emission and CO2 Emission per Person**
+
+    // **Task 5: Calculate middle geographical point and average forested area for group of countries with the same official language**
+
+    // **Task 6: Find the best option to visualize this dataframe among charts available with Kandy**
 }
